7b74e4c50e38aaecd8d682bfc68a5ad03840388d
Edson Domingos da Silva <e.domingos@academico.ifrn.edu.br>
2022-12-16 10:22:14 -0300

    update

--- ca/App.js
+++ ca/App.js
@@ -3,16 +3,18 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './src/components/Login'
 import Index from './src/components/Index'
+import { Text } from 'react-native'
 
 const Stack = createNativeStackNavigator()
 
 export default () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initial='Index' screenOptions={{ headerShown: true}}>
-        <Stack.Screen name='index' component={Index} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    /* <NavigationContainer>
+    //   <Stack.Navigator initial='Index' screenOptions={{ headerShown: true}}>
+    //     <Stack.Screen name='index' component={Index} />
+    //   </Stack.Navigator>
+    // </NavigationContainer> */
+    <Text>Teste</Text>
   );
 }
 
--- ca/src/styledComponents/Index.js
+++ ca/src/styledComponents/Index.js
@@ -2,6 +2,7 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
     display: flex;
+    flex: 1;
     flex-direction: column
 `;
 
