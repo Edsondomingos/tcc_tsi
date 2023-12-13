9b53f4ce6361b2007fcfc60f57d9f4eed4734646
Edson Domingos <edomingoswork@gmai.com>
2023-12-13 16:38:48 -0300

    Iniciado

--- ca/app/index.js
+++ ca/app/index.js
@@ -1,20 +1,105 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Container1,
+  Container2,
+  Container3,
+  ContainerVaga,
+  SmallContainer,
+  ContainerRow,
+  SubTitle,
+  TextNormal,
+  Btn,
+  Btn2,
+  TextBtn,
+  Inputs,
+  Lists,
+  Scrolls,
+  Logar,
+} from './style'
+import { useState } from 'react'
+
+let conteudo = [
+  {
+    vaga: 'Desenvolvedor mobile jr',
+    tipo: 'CLT',
+    remuneracao: 2000,
+    requisitos: 'conhecimentos em react native',
+  },
+  {
+    vaga: 'Desenvolvedor Python',
+    tipo: 'CLT',
+    remuneracao: 3000,
+    requisitos: 'Desenvolvimento em POO',
+  },
+  {
+    vaga: 'Desenvolvedor Python',
+    tipo: 'CLT',
+    remuneracao: 3000,
+    requisitos: 'Desenvolvimento em POO',
+  },
+  {
+    vaga: 'Desenvolvedor Python',
+    tipo: 'CLT',
+    remuneracao: 3000,
+    requisitos: 'Desenvolvimento em POO',
+  },
+  {
+    vaga: 'Desenvolvedor Python',
+    tipo: 'CLT',
+    remuneracao: 3000,
+    requisitos: 'Desenvolvimento em POO',
+  },
+  {
+    vaga: 'Desenvolvedor Python',
+    tipo: 'CLT',
+    remuneracao: 3000,
+    requisitos: 'Desenvolvimento em POO',
+  },
+  {
+    vaga: 'Desenvolvedor Python',
+    tipo: 'CLT',
+    remuneracao: 3000,
+    requisitos: 'Desenvolvimento em POO',
+  },
+  {
+    vaga: 'Desenvolvedor Python',
+    tipo: 'CLT',
+    remuneracao: 3000,
+    requisitos: 'Desenvolvimento em POO',
+  },
+];
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>DENTRO INDEX.js</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container1>
+      <Container2>
+        <Btn2>
+          <Logar
+            source={require('./imgs/perfilBranco.png')}
+          />
+        </Btn2>
+
+      </Container2>
+
+      <Container3>
+        <Scrolls>
+          <Lists
+            data={conteudo}
+            renderItem={({ item }) => {
+              return (
+                <ContainerVaga>
+                  <SubTitle>SubTitle</SubTitle>
+                  <TextNormal>Vaga</TextNormal>
+                  <TextNormal>Tipo</TextNormal>
+                  <TextNormal>+ Salário: </TextNormal>
+                  <TextNormal>+ Empresa:</TextNormal>
+                </ContainerVaga>
+              );
+            }}
+          />
+        </Scrolls>
+      </Container3>
+    </Container1>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
