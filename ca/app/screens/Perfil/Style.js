ac11dd71231a8be72d97db130be4a23a3039d3c2
Edson Domingos <edomingoswork@gmai.com>
2024-04-09 13:37:56 -0300

    criacao login e signup, melhorias em perfil

--- ca/app/screens/Perfil/Style.js
+++ ca/app/screens/Perfil/Style.js
@@ -1,5 +1,29 @@
 import { StyleSheet } from "react-native"
 
 export default StyleSheet.create({
-    
+    container: {
+        flex: 1,
+    },
+    containerUser: {
+        display: 'flex', 
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    imgPerfil: {
+        width: 150, 
+        height: 150,
+        borderRadius: 100,
+        borderWidth: 1, 
+        borderColor: '#000',
+        margin: 10,
+
+    },
+    user: {
+        fontSize: 18,
+    },
+    titulos: {
+        fontSize: 25,
+        borderBottomWidth: 1,
+        borderColor: '#000',
+    }
 })
--- ca/app/screens/Perfil/index.js
+++ ca/app/screens/Perfil/index.js
@@ -1,29 +1,37 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, ScrollView } from 'react-native'
 import Style from './Style'
+import Menu from '../../components/Menu'
 
 export default Perfil = () => {
     return (
-        <View>
-            <View>
-                <Text>Nom usuario</Text>
-                <Text>Casado, 29 anos</Text>
-                <Text>Endereço completo</Text>
-                <Text>(84) 99999-9999</Text>
-                <Text>Redes Sociais:</Text>
-            </View>
+        <View style={Style.container}>
+            <ScrollView>
+                <View style={Style.containerUser}>
+                    <Image source={require('../../imgs/perfilBranco.png')}
+                        style={Style.imgPerfil} />
+                    <View>
+                        <Text style={Style.user}>Nom usuario</Text>
+                        <Text style={Style.user}>Casado, 29 anos</Text>
+                        <Text style={Style.user}>Endereço completo</Text>
+                        <Text style={Style.user}>(84) 99999-9999</Text>
+                        <Text style={Style.user}>Redes Sociais:</Text>
+                    </View>
+                </View>
 
-            <View>
-                <Text>Formação</Text>
-            </View>
+                <View>
+                    <Text style={Style.titulos}>Formação</Text>
+                </View>
 
-            <View>
-                <Text>Experiência Profisional</Text>
-            </View>
+                <View>
+                    <Text style={Style.titulos}>Experiência Profisional</Text>
+                </View>
 
 
-            <View>
-                <Text>Pitch</Text>
-            </View>
+                <View>
+                    <Text style={Style.titulos}>Pitch</Text>
+                </View>
+            </ScrollView>
+            <Menu />
         </View>
     )
 }
